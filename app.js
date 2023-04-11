be6a297fef53e9ae5e0c22637b96b350335d3df4
tharunoptimus <tharunoptimus@outlook.com>
2023-04-12 00:59:30 +0530

    Added function `saveToFile`

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+import fs from "fs"
+
 let url = (movieName) => {
 	return `https://www.tamil2lyrics.com/movies/${movieName}/`
 }
 
+
+async function saveToFile(songName, lyricsContent) {
+	const folderName = `songs`
+	const fileName = `${folderName}/${songName}`
+
+	lyricsContent = lyricsContent.join("")
+
+	fs.writeFile(fileName, lyricsContent, (err) => {
+		if (err) throw err
+		console.log(`Saved ${songName}!`)
+	})
+}
