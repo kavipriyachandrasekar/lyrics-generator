998a695875ce94573c093c69f94f2384e4e6781a
tharunoptimus <tharunoptimus@outlook.com>
2023-04-12 03:03:43 +0530

    Puppeteer script to download lyrics

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
+import puppeteer from "puppeteer"
 import fs from "fs"
 
 let url = (movieName) => {
 	return `https://www.tamil2lyrics.com/movies/${movieName}/`
 }
 
+let getLyrics = async (url) => {
+	const browser = await puppeteer.launch({ headless: false })
+	const page = await browser.newPage()
+	await page.goto(url)
+
+	const allLinks = await page.evaluate(() => {
+		const links = document.querySelectorAll(
+			"div.col-lg-6.col-sm-6.col-xs-6 > a"
+		)
+		const linksArray = [...links]
+		const linksArray2 = linksArray.map((link) => link.href)
+		return linksArray2
+	})
+
+	for (let i = 0; i < allLinks.length; i++) {
+		const newPage = await browser.newPage()
+		await newPage.goto(allLinks[i])
+		const lyrics = await newPage.evaluate(() => {
+			let lyrics = document.querySelectorAll("div#Tamil > p")
+			lyrics = [...lyrics]
+			lyrics.splice(0, 2)
+			lyrics.forEach((lyric) => {
+				lyric.removeChild(lyric.firstChild)
+			})
+
+			const lyricsArray = [...lyrics]
+			return lyricsArray.map((lyric) => lyric.innerText)
+		})
+		console.log(lyrics)
+		let fileNameArray = allLinks[i].split("/")
+		let fileName = fileNameArray[fileNameArray.length - 2]
+		saveToFile(fileName + ".txt", lyrics)
+
+		await newPage.close()
+	}
+
+	await browser.close()
+}
+
+getLyrics(url("24"))
 
 async function saveToFile(songName, lyricsContent) {
 	const folderName = `songs`
